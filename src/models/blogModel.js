fba875ebeac3f05a27da5498921f3d1d33a01e5f
Alfiya <alfiya96960@gmail.com>
2023-01-02 18:21:55 +0530

    add put API

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,41 +1,39 @@
-const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const blogSchema = new mongoose.Schema( {
-    title: {
-        type:String,
-        require: true,
-    body: {
-        type: String,
-        required: true
-    },
-    authorId: {
-        type: ObjectId,
-        required: true,        
-        ref: "authData",
-    },
-    tags: [String],
-    category: {
-        type: String,
-        required: true
-    },
-    subcategory:[String],
-    isDeleted :{
-        type:Boolean,
-        default: false,
-        date : Date.now()
-    },
-    
-    publishedAt:{
-     date : Date.now(),
-    },
+const blogSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            require: true,
+            body: {
+                type: String,
+                required: true,
+            },
+            authorId: {
+                type: ObjectId,
+                required: true,
+                ref: "authData",
+            },
+            tags: [String],
+            category: {
+                type: String,
+                required: true,
+            },
+            subcategory: [String],
+            isDeleted: {
+                type: Boolean,
+                default: false,
+                date: Date.now(),
+            },
 
-    isPublished  :{
-    type:Boolean,
-    default: false,
-    
+            isPublished: {
+                type: Boolean,
+                default: false,
+            },
+        },
     },
-}},{ timestamps: true });
+    { timestamps: true }
+);
 
-
-module.exports = mongoose.model('blogData', blogSchema)
+module.exports = mongoose.model("blogData", blogSchema);
