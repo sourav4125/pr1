06df82dbf12aedbb59bedb0acc0045c605f7f478
amankr1635 <amankr.1635@gmail.com>
2023-01-02 12:57:10 +0530

    Author model 01

---
+++ authorModel/authorModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const email = require("mongoose-type-email");
+const password = require("password");
+const authorSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        require: true
+    },
+    lname: {
+        type: String,
+        require: true
+    },
+    title: {
+        type: String,
+        enum: ["Mr", "Mrs", "Miss"],
+        require: true
+    }, email: {
+        type: email,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: password,
+        require: true
+
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('authData', authorSchema) 
